2a776c3cb6094c67aec5994045452bb7207ddcee
Vinicius Lima <kiddlima@outlook.com>
2018-07-12 21:23:49 -0300

    Refatoração de schema de categoria para fase de obra

--- schema/category.schema.js
+++
@@ -1,9 +0,0 @@
-const mongoose = require('mongoose');
-
-const requiredMessage = " é obrigatório";
-
-let category = new mongoose.Schema({
-    name: {type: String, required: [true, 'Nome da categoria' + requiredMessage], unique: true}
-}, {versionKey: false});
-
-module.exports = mongoose.model('Category', category);
